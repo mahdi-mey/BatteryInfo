e40b4e8f194cdc979a48083e0be99ac95e040188
Mahdi <mahdimey17@gmail.com>
2023-11-10 22:55:53 +0330

    added battery persentage functionality

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+const batteryText = document.querySelector('.battery-percentage .real')
+const batteryBoxes = document.querySelectorAll('.real-battery > *')
+
+const colorVariableInCss = window.getComputedStyle(document.documentElement).getPropertyValue('--real-color')
 
 window.addEventListener('load', () => {
-    console.log(navigator.getBattery)
+    navigator.getBattery().then(batteryInfo => {
+        const batteryLevel =  Math.round(batteryInfo.level * 100)
+        batteryText.innerHTML = `${batteryLevel}%`
+
+        const batteryammount = Math.round(batteryInfo.level * 10)
+        for(let i = 0; i < batteryammount; i++){
+            batteryBoxes[i].style.backgroundColor = colorVariableInCss
+        }
+    })
 })
