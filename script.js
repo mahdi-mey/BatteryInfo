dad4137d5a2e6f848aea47f4e2b953bbf1b669c1
Mahdi <mahdimey17@gmail.com>
2023-11-11 17:14:27 +0330

    added charger icon color when pluged in

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const batteryText = document.querySelector('.battery-percentage .real')
 const batteryBoxes = document.querySelectorAll('.real-battery > *')
+const chargerIcon = document.querySelector('.chargerIcon')
 
 const mainColor = window.getComputedStyle(document.documentElement).getPropertyValue('--real-color')
 const secondaryColor = window.getComputedStyle(document.documentElement).getPropertyValue('--fade-color')
 
 window.addEventListener('load', () => {
     navigator.getBattery().then(batteryInfo => {
+        console.log(batteryInfo)
+
+        if(batteryInfo.charging){
+            chargerIcon.style.color = mainColor
+        }
+        else{
+            chargerIcon.style.color = secondaryColor
+        }
+
         const batteryLevel =  Math.round(batteryInfo.level * 100)
         batteryText.innerHTML = `${batteryLevel}%`
 
