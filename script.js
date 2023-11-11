01aa827d464423354e1f4956e626c14225b511d2
Mahdi <mahdimey17@gmail.com>
2023-11-11 18:01:04 +0330

    added new functions for every event

--- script.js
+++ script.js
@@ -5,26 +5,47 @@ const chargerIcon = document.querySelector('.chargerIcon')
 const mainColor = window.getComputedStyle(document.documentElement).getPropertyValue('--real-color')
 const secondaryColor = window.getComputedStyle(document.documentElement).getPropertyValue('--fade-color')
 
-window.addEventListener('load', () => {
+function batteryInformation(){
     navigator.getBattery().then(batteryInfo => {
         console.log(batteryInfo)
-
-        // battery icon color
-        if(batteryInfo.charging){
-            chargerIcon.style.color = mainColor
-        }
-        else{
-            chargerIcon.style.color = secondaryColor
-        }
-
-        // battery persentage handler
-        const batteryLevel =  Math.round(batteryInfo.level * 100)
-        batteryText.innerHTML = `${batteryLevel}%`
-
-        // changes battery boxes
-        const batteryammount = Math.round(batteryInfo.level * 10)
-        for(let i = 0; i < batteryammount; i++){
-            batteryBoxes[i].style.backgroundColor = mainColor
-        }
+        batteryIcon(batteryInfo)
+        batteryPercentage(batteryInfo)
+        batteryBoxesHandler(batteryInfo)
+        batteryPlugChage(batteryInfo)
     })
-})
+}
+
+function batteryIcon(batteryInfo){
+    // battery icon color
+    if(batteryInfo.charging){
+        chargerIcon.style.color = mainColor
+    }
+    else{
+        chargerIcon.style.color = secondaryColor
+    }
+}
+function batteryPercentage(batteryInfo){
+    // battery persentage handler
+    const batteryLevel =  Math.round(batteryInfo.level * 100)
+    batteryText.innerHTML = `${batteryLevel}%`
+}
+function batteryBoxesHandler(batteryInfo){
+    // changes battery boxes
+    const batteryammount = Math.round(batteryInfo.level * 10)
+    for(let i = 0; i < batteryammount; i++){
+        batteryBoxes[i].style.backgroundColor = mainColor
+    }
+}
+function batteryPlugChage(batteryInfo){
+    batteryInfo.addEventListener('chargingchange', function(event) {
+        if (batteryInfo.charging) {
+          console.log("Charger plugged in");
+          batteryIcon(batteryInfo)
+        } else {
+          console.log("Charger unplugged");
+          batteryIcon(batteryInfo)
+        }
+      });
+}
+
+window.addEventListener('load', batteryInformation)
